a0c9558ff357bf5deec47fd4c269472f39e6f1c6
JongoDB <Jonathanrannabargar@gmail.com>
2026-02-01 17:16:02 -0500

    feat(onboarding): add index exports

---
+++ packages/frontend/src/components/onboarding/index.ts
@@ -0,0 +1,6 @@
+export { OnboardingTour } from './OnboardingTour';
+export { WelcomeModal } from './WelcomeModal';
+export { TourSection } from './TourSection';
+export { TourToast } from './TourToast';
+export { TOUR_STEPS, TOUR_STORAGE_KEYS } from './tourSteps';
+export type { TourStep } from './tourSteps';
