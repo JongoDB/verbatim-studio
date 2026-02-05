8f9fa50bfde64d03c7dedc670f17733c1653f6d2
JongoDB <Jonathanrannabargar@gmail.com>
2026-02-04 22:31:05 -0500

    chore: bump version to 0.31.0

--- packages/frontend/src/version.ts
+++ packages/frontend/src/version.ts
@@ -1,3 +1,3 @@
 // This file is updated automatically by the release process
 // Run: npm run update-version or ./scripts/update-version.sh
-export const APP_VERSION = 'v0.30.1';
+export const APP_VERSION = 'v0.31.0';
